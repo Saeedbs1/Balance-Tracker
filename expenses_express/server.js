32b4285653f5c77471c208dfb604b5f593a067d0
Saeedbs1 <saeedbs2001.1@gmail.com>
2025-08-06 22:46:59 +0300

    fix

--- expenses_express/server.js
+++ expenses_express/server.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const SECRET = process.env.JWT_SECRET;
 app.use(cors());
 app.use(bodyParser.json());
